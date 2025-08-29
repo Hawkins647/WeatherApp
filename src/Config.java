4a570e00d2cabf13078add01f7edab8501c0eb15
Hawkins647 <59938776+Hawkins647@users.noreply.github.com>
2025-08-29 22:55:57 +0100

    Update Config.java

--- src/Config.java
+++ src/Config.java
@@ -1,6 +1,6 @@
 public class Config {
 
-    public static String apiKey = "9fa1bec7a81d4b137469e268f2c7ce8b";
+    public static String apiKey = "123";
 
     public static String getKey() {
         return apiKey;
