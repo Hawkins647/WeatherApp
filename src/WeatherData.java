fe691328db9ada7ec8c7a6155012b9ee9876683f
Hawkins647 <59938776+Hawkins647@users.noreply.github.com>
2025-09-07 22:15:56 +0100

    Update WeatherData.java
    Created new getWeather functions

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -9,16 +9,70 @@ public class WeatherData {
     private static String apiKey = Config.getKey();
 
     public static void main(String[] args) {
-        HttpResponse<String> testResponse = getWeather("London", "metric");
+        HttpResponse<String> testResponse = getWeatherByCity("London", "metric");
         System.out.println(testResponse.body());
+
+        HttpResponse<String> testResponse2 = getWeatherByCoordinates("40.19",  "54.02", "metric");
+        System.out.println(testResponse2.body());
+
+        HttpResponse<String> testResponse3 = getWeatherByPostcode("NR4", "GB", "metric");
+        System.out.println(testResponse3.body());
+
     }
 
-    //TODO: overload method for entry for postcode, coordinates
-    public static HttpResponse<String> getWeather(String city, String units) {
+    public static HttpResponse<String> getWeatherByCity(String city, String units) {
+        /* Create an API request based off a city input, return the data */
 
         try {
             String requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey +
                     "&units=" + units;
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(requestURL)).GET().build();
+
+            HttpResponse<String> weatherResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return weatherResponse;
+
+        } catch (IOException e){
+            System.out.println("IOException");
+        } catch (InterruptedException e ) {
+            System.out.println("Interrupted Exception");
+        }
+
+        return null;
+    }
+
+
+    public static HttpResponse<String> getWeatherByCoordinates(String latitude, String longitude, String units) {
+        /* Create an API request based off a latitude and longitude input, return the data */
+
+        try {
+            String requestURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude
+                    + "&appid=" + apiKey + "&units=" + units;
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(requestURL)).GET().build();
+
+            HttpResponse<String> weatherResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return weatherResponse;
+
+        } catch (IOException e){
+            System.out.println("IOException");
+        } catch (InterruptedException e ) {
+            System.out.println("Interrupted Exception");
+        }
+
+        return null;
+    }
+
+    public static HttpResponse<String> getWeatherByPostcode(String postcode, String countryCode, String units) {
+        /* Create an API request based off a postcode input, return the data */
+
+        try {
+            String requestURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + postcode + "," + countryCode
+                    + "&appid=" + apiKey + "&units=" + units;
 
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(requestURL)).GET().build();
